d48d36d2a8f2e41821130e9ddc30e59fd8985cbf
Aryan Karekar <77088155+syswraith@users.noreply.github.com>
2022-08-06 23:54:43 +0530

    if else ==> switch
    added the promised switch statements, did not fix the bug encountered but made efforts. maybe ask vampirejs?

--- eventlisteners.js
+++ eventlisteners.js
@@ -1,20 +1,36 @@
 var inputCmnd = document.getElementById("inputCmnd");
-var historyKey = 0;
+const historyKey = 0;
+var upDwn = historyKey;
 var historyVal;
 
+// bug in the up and down events
+
 inputCmnd.addEventListener("keydown", function(event) {
-    if (event.key === "Enter"){                                // Add a switch statement to make this cleaner
-        historyKey  = ++historyKey;                           // Here you increment the index to 0
-        historyVal = inputCmnd.value;                        // Here you set the value equal to value
-        historyVal = historyVal.toLowerCase();              // Here you convert it to lower case
-        sessionStorage.setItem(historyKey, historyVal);
-        inputCmnd.value = "";
+    switch(event.key) {
+        case "Enter":
+            upDwn = ++upDwn;
+            historyVal = inputCmnd.value;
+            historyVal = historyVal.toLowerCase;
+            sessionStorage.setItem(upDwn, historyVal);
+            inputCmnd.value = "";
+            break;
+        case "ArrowUp":
+            if (upDwn > historyKey){
+                inputCmnd.value = sessionStorage.getItem(--upDwn);
+            }
+            else if (upDwn == 1){
+                inputCmnd.value = "lol cant go back"
+            }
+            else { console.log("ERR") }
+            break;
+        case "ArrowDown":
+            if(upDwn < sessionStorage.length){
+                inputCmnd.value = sessionStorage.getItem(++upDwn);
+            }
+            else if (upDwn == sessionStorage.length){
+                inputCmnd.value = "lol cant go further"
+            }
+            else { console.log("ERR") }
+            break;
     }
-    else if (event.key === "ArrowUp") {
-        inputCmnd.value = sessionStorage.getItem(--historyKey);
-    } 
-    else if (event.key === "ArrowDown") {
-        inputCmnd.value = sessionStorage.getItem(++historyKey);
-    }
-    else {}
 });
