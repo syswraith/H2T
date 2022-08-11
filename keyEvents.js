1878370386fd33929e8a6086cf9de03daa360ec8
Aryan Karekar <77088155+syswraith@users.noreply.github.com>
2022-08-11 14:15:09 +0530

    Update keyEvents.js

--- keyEvents.js
+++ keyEvents.js
@@ -1,32 +1,52 @@
-inputCmnd.addEventListener("keydown", function(event) {
-    switch(event.key) {
-        case "Enter":
-            comparevar = inputCmnd.value;
-            commandCompare(comparevar);
-            historyVal = inputCmnd.value;
-            sessionStorage.setItem(upDwn, historyVal);
-            upDwn = ++upDwn;
-            inputCmnd.value = "";
+function commandCompare(comparevar){
+    switch(comparevar){
+        case "help":
+            var newCommand = document.createElement("li");
+            newCommand.innerHTML = "help: You can use the following commands:<pre> whoami     hostname     history<br> date       history      ls<br> uname      reboot<br><br> Alternatively, you can use command help with 'help [command]'</pre>";
+            appendSpace.appendChild(newCommand);
             break;
-        case "ArrowUp":
-            if (upDwn > historyKey){
-                inputCmnd.value = sessionStorage.getItem(--upDwn);
-                
-            }
-            else if (upDwn == 1){
-                inputCmnd.value = sessionStorage.getItem(upDwn);
-                console.log("ERR: back not possible");
-            }
-            else { console.log("ERR: Unknown") }
+
+        case "whoami":
+            var newCommand = document.createElement("li");
+            newCommand.innerHTML = "whoami<br>flynn";
+            appendSpace.appendChild(newCommand);
             break;
-        case "ArrowDown":
-            if(upDwn < sessionStorage.length){
-                inputCmnd.value = sessionStorage.getItem(++upDwn);
-            }
-            else if (upDwn == sessionStorage.length){
-                console.log("ERR: further not possible");
-            }
-            else { console.log("ERR: Unknown") }
+
+        case "clear":
+            appendSpace.innerHTML = "";
             break;
+
+        case "ls":
+            break;
+
+        case "date":
+            var newCommand = document.createElement("li");
+            newCommand.innerHTML = "date<br>" +current.toLocaleString();
+            appendSpace.appendChild(newCommand);
+            current.getFullYear();
+            break;
+
+        case "history":
+            var historyGet = 0;
+            while (historyGet != sessionStorage.length) {
+                var newCommand = document.createElement("li");
+                newCommand.innerHTML = sessionStorage.getItem(historyGet);
+                appendSpace.appendChild(newCommand);
+                historyGet++;
+            }
+            break;
+
+        case "uname":
+            var newCommand = document.createElement("li");
+            newCommand.innerHTML = "uname<br>H2T: Hyper Text Terminal<br> made by syswraith";
+            appendSpace.appendChild(newCommand);
+            break;
+
+        default:
+            var newCommand = document.createElement("li");
+            newCommand.innerHTML = comparevar + " is not a valid command. Type help for more info.";
+            appendSpace.appendChild(newCommand);
+            break;
+
     }
-});
+}
