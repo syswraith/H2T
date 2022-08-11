f8973d34af0db5b57b850dbf0e4092a88060c18b
Aryan Karekar <77088155+syswraith@users.noreply.github.com>
2022-08-11 14:14:39 +0530

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
 const inputCmnd = document.getElementById("inputCmnd");
 const historyKey = 1;
-var comparevar;
+const appendSpace = document.getElementById("appendSpace");
 var upDwn = historyKey;
 var historyVal;
-var command;
+var current = new Date();
